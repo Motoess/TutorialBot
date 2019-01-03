7f8633ed539ea13f5b8aaa0b213cf3918c564117
Warhawk <devpure1@gmail.com>
2019-01-03 15:53:31 -0800

    EP2 Files

---
+++ server.js
@@ -0,0 +1,38 @@
+// Packages Needed
+const Discord = require('discord.js');
+const client = new Discord.Client()
+let tokenfile = require('./token.json');
+
+// Variables
+const prefix = '=';
+const ownerID = '206587228200239104';
+
+// Listener Events
+client.on('message', message => {
+
+    // Variables
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    // Return Statements
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // Command Handler
+    try {
+
+        delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+        
+        let commandFile = require(`./commands/${cmd}.js`);
+        commandFile.run(client, message, args);
+
+    } catch (e) {
+        console.log(e.stack)
+    }
+
+});
+
+client.on('ready', () => console.log('Launched'));
+
+// Login to Discord
+client.login(tokenfile.TOKEN);
