152789e40864bc7242c0800a4c7878e1fe3078ea
Warhawk <devpure1@gmail.com>
2019-01-16 15:55:26 -0800

    Ascii Command

--- commands/ascii.js
+++ commands/ascii.js
@@ -1,16 +1,15 @@
 const ascii = require('ascii-art');
 
-// Command Handler - You can use your own if you want
-exports.run = (client, message, args, ops) => {
-
-    // First, we need to actually generate the font the command will be using
+// Command Handler
+exports.run = (client, message, args) => {
+    
+    // First, we need to actually generate the font the command will use
     ascii.font(args.join(' '), 'Doom', function(rendered) {
-        // args hold the array of the words following the command, so if we join them we get a string of words following the command
+        // args holds the array of the words following the command, so if we join them we get a string of words following the command
 
         // The `rendered` variable contains our output
-        // Although, there is usually a few spaces on the end
-        rendered = rendered.trimRight(); // This will remove the whitespace on the right side of the string
-        
+        // Although, there is usualy a few spaces on the end
+        rendered = rendered.trimRight(); // This will remove the white space on the right side of the string
 
         // Now, we need to check if the string exceeds the max characters
         if (rendered.length > 2000) return message.channel.send('Sorry, that message is too long!');
@@ -18,6 +17,6 @@ exports.run = (client, message, args, ops) => {
         // Finally, if it didn't return any error, send the message
         message.channel.send(rendered, {
             code: 'md'
-        }); // The code option specifies that the message should be in a code block
+        }); // The code option at the end specifies that the message should be in a code block
     });
 };
